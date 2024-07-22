af21a4efe5fff4da9457c2966c71f1c631c8864f
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-22 20:24:52 +0530

    initial commit of read content from the file

---
+++ ReadContent/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ReadContentFromFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string textFilePath = "C:\\Users\\swati.padmanabhan\\Desktop\\Training Weeks\\" +
+                                        "2ndWeek\\ReadContentFromFile\\TextFiles\\Fruits.txt";
+
+            const string htmlFilePath = "C:\\Users\\swati.padmanabhan\\Desktop\\Training Weeks\\" +
+                                        "2ndWeek\\ReadContentFromFile\\HTMLFiles\\Sample.html";
+
+            ReadContentFromFiles(textFilePath, "Text File");
+            ReadContentFromFiles(htmlFilePath, "HTML File");
+
+        }
+
+        static void ReadContentFromFiles(string filePath, string fileType)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            string line = reader.ReadLine();
+            Console.WriteLine($"====================Reading Content from {fileType}====================");
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+            }
+
+        }
+
+    }
+}
