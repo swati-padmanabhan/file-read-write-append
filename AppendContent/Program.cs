dd882d82ff6ae839daf8b714abdfe67424417b12
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-22 20:29:17 +0530

    add append content to file

---
+++ AppendContent/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AppendContentsToFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string path = "C:\\Users\\swati.padmanabhan\\Desktop\\Training Weeks\\" +
+                                "2ndWeek\\AppendContentsToFile\\TextFiles\\Username.txt";
+
+            Console.WriteLine("Enter your username: ");
+            string username = Console.ReadLine();
+
+            AppendContentToFile(path, username);
+
+            Console.WriteLine("Username Added Successfully");
+
+        }
+        static void AppendContentToFile(string path, string username)
+        {
+            //opening the file in append mode
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                writer.WriteLine(username);
+            }
+        }
+    }
+}
