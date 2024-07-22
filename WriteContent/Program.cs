eb779438d8c6a51805b4d76e8fb7f239751f1d48
Swati P <67189695+swati-padmanabhan@users.noreply.github.com>
2024-07-22 20:27:54 +0530

    add writing content to file

---
+++ WriteContent/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace WriteContentToFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteContentToTextFile();
+            WriteContentToHTMLFile();
+        }
+
+        //Method to write weather conditions in text file
+        static void WriteContentToTextFile()
+        {
+            //File path for text file
+            const string textFilePath = "C:\\Users\\swati.padmanabhan\\Desktop\\Training Weeks\\" +
+                                "2ndWeek\\WriteContentToFile\\TextFiles\\Weather.txt";
+
+            //StreamWriter to write to this file
+            StreamWriter writer = new StreamWriter(textFilePath);
+
+            string[] contentOnText = { "Sunny", "Rainy", "Cloudy", "Windy", "Snowy" };
+
+            foreach (string content in contentOnText)
+            {
+                writer.WriteLine(content);
+            }
+            Console.WriteLine("Text File Added Successfully !");
+            writer.Close();
+        }
+
+
+        //Method to write weather conditions in HTML file
+        static void WriteContentToHTMLFile()
+        {
+            const string path = "C:\\Users\\swati.padmanabhan\\Desktop\\Training Weeks\\" +
+                               "2ndWeek\\WriteContentToFile\\TextFiles\\Weather.html";
+            string htmlContent = @"
+                                <html>
+                                <head>
+                                    <title>Weather Report</title>
+                                </head>
+                                <body>
+                                    <h1>Weather Conditions</h1>
+                                    <ul>
+                                        <li>Sunny</li>
+                                        <li>Rainy</li>
+                                        <li>Cloudy</li>
+                                        <li>Windy</li>
+                                        <li>Snowy</li>
+                                    </ul>
+                                </body>
+                                </html>";
+            //Write the HTML content to the file
+            File.WriteAllText(path, htmlContent);
+            Console.WriteLine("HTML File Added Successfully !");
+        }
+    }
+}
